c888019720bf91107e018ccb77d127b43511bf12
DionEngels <d.j.engels@student.tue.nl>
2020-11-15 10:34:57 +0100

    Ex6 v2

--- Set 1/Ex6.py
+++ Set 1/Ex6.py
@@ -9,6 +9,7 @@ Project: ScientificProgramming
 File: Ex6
 """
 import numpy as np
+from Ex2 import ex2a
 
 
 def ex6a(array):
@@ -19,13 +20,9 @@ def ex6b(array):
     return np.where(array == np.min(array))[0]
 
 
-def ex6c(array, max_val):
-    return [len(array[array == value]) for value in range(1, max_val)]
-
-
 k = 9
 v = np.random.randint(low=1, high=k, size=10**7)
 
 print(f'a) Result \t: {ex6a(v)}')
 print(f'b) Result \t: {ex6b(v)}')
-print(f'c) Result \t: {ex6c(v, k)}')
+print(f'c) Result \t: {ex2a(k-1, v)}')
