6f63c58bd9749cffaf4dcb3a8221ac3b1e758875
joris willems <willems@JorisImac.local>
2020-11-17 11:43:17 +0100

    solutions set 1, some course mataterial added

---
+++ Set 1/given_material/__init__.py
@@ -0,0 +1,5 @@
+from .intro import *
+from .mlext import *
+from .plot_graph import *
+from .reorder import *
+from .square_triangular_grid import *
--- Set 1/given_material/intro.py
+++ Set 1/given_material/intro.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 print(np.matrix('1 2; 3 4'))
 print(np.matrix([[1, 2], [3, 4]]))
 
