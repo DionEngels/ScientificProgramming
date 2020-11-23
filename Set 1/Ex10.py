359ffd9d8164fc1ed0077fefb48ea24a271eb23d
DionEngels <d.j.engels@student.tue.nl>
2020-11-23 14:56:43 +0100

    Ex10 working

--- Set 1/Ex10.py
+++ Set 1/Ex10.py
@@ -12,17 +12,19 @@ import numpy as np
 from given_material.mlext import accumarray
 
 
-def ex10_v2(N):
-    ones = (N == (np.max(N[N[:, 1] == 1, 0]), 1)).all(axis=1)
-    twos = (N == (np.max(N[N[:, 1] == 2, 0]), 2)).all(axis=1)
-    threes = (N == (np.max(N[N[:, 1] == 3, 0]), 3)).all(axis=1)
-    fours = (N == (np.max(N[N[:, 1] == 4, 0]), 4)).all(axis=1)
-    return N[ones | twos | threes | fours, :]
+def ex10(H):
+    ones = (H == (np.max(H[H[:, 1] == 1, 0], initial=0), 1)).all(axis=1)
+    twos = (H == (np.max(H[H[:, 1] == 2, 0], initial=0), 2)).all(axis=1)
+    threes = (H == (np.max(H[H[:, 1] == 3, 0], initial=0), 3)).all(axis=1)
+    fours = (H == (np.max(H[H[:, 1] == 4, 0], initial=0), 4)).all(axis=1)
+    return H[ones | twos | threes | fours]
 
-# define
+# define random if you want
 n = 14
 ranks = np.random.randint(low=1, high=14, size=n)
 suites = np.random.randint(low=1, high=5, size=n)
-N = np.transpose(np.vstack((ranks, suites)))
-print(ex10_v2(N))
+H = np.transpose(np.vstack((ranks, suites)))
+# define example
+H = np.array([[7, 1], [3, 2], [6, 1], [4, 2], [4, 2], [7, 1], [3, 4]])
+print(ex10(H))
 
